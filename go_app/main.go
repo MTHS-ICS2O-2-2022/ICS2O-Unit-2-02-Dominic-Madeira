9ec2b6f8ad8933a577f7b9358524d844d369f873
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-03-09 18:30:26 +0000

    added go

---
+++ go_app/main.go
@@ -0,0 +1,18 @@
+// Created by: Dominic M.
+// Created on: March 2023
+//
+// This program does basic math
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// This function calculates area and perimeter of a rectangle with dimensions 5cm x 3cm
+	fmt.Println("The perimeter is: ", (2 * (5 + 3)))
+	fmt.Println("The area is: ", (5 * 3) "cm²",)
+
+  fmt.Println("\nDone.")
+}
