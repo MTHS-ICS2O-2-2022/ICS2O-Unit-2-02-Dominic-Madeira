fb243792f6a2fee2fcb1991ae69f203f1dc7a7b8
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-03-09 18:13:38 +0000

    first edit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Dominic M. All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Dominic M.
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function doMathClicked() {
+  document.getElementById("add-perimeter").innerHTML = "<p>The perimeter is: " + (4 + 4 + 6 + 6) + " cm.</p>"
+  document.getElementById("multiply-area").innerHTML = "<p>The area is: " + (4 *6) + " cm².</p>"
+}
